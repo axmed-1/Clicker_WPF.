53f2bdae7e0583fe2186c5284b694f5be9a810e2
axmed <mikailov_akhmed@bk.ru>
2025-11-10 12:23:35 +0300

    refactoring

--- Clicker_WPF/MainWindow.xaml.cs
+++ Clicker_WPF/MainWindow.xaml.cs
@@ -3,16 +3,33 @@ namespace Clicker_WPF_
 {
     public partial class MainWindow : Window
     {
-        private int counter = 0;
+        private int _counter = 0;
         public MainWindow()
         {
             InitializeComponent();
+            UpdateCounterText();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            counter++;
-            CounterText.Text = $"Счетчик:{counter}";
+            _counter++;
+            CounterText.Text = $"Счетчик:{_counter}";
+            UpdateCounterText();
         }
+        private void Button_Reset(object sender, RoutedEventArgs e)
+        {
+            _counter = 0;
+            UpdateCounterText();
+        }
+        private void UpdateCounterText()
+        {
+            CounterText.Text = $"Счетчик:{_counter}";
+        }
+        
+
+       
+
+        
+        }
+
     }
-}
